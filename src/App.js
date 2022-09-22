a1050d10c654bbb151e33f9631d3fa2815b0008a
StefS99 <stefan.simic9977@gmail.com>
2022-09-22 08:57:20 +0200

    Izvor podataka

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import About from './components/About';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import {useState} from "react";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
+  const [likeTotal, setlikeTotal] = useState(0);
+  const [posts] = useState([
+    {
+      id: 1,
+      title: "Prvi post",
+      description:
+        "Ovo je prvi post u React applikaciji",
+      likes:0
+    },
+    {
+      id: 2,
+      title: "Drugi post",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum in cumque rerum numquam fugit id error, cum fugiat porro.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum in cumque rerum numquam fugit id error, cum fugiat porro.",
+      likes:0
+    },
+    {
+      id: 3,
+      title: "Treci post",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum in cumque rerum numquam fugit id error, cum fugiat porro.",
+      likes:0
+    }
+  ]);
+
+  function like(id){
+    posts.forEach((post) => {
+      if(post.id === id){
+        post.likes = post.likes+1;
+        setlikeTotal(likeTotal+1);
+        logPosts();
+      }
+    });
+  }
+
+  function logPosts(){
+    posts.forEach((post) =>{
+      console.log(post.id +" "+post.likes);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter className="App">
+    <NavBar></NavBar>
+    <Routes>
+      <Route
+        path="/"
+        element={<Main posts = {posts} likeTotal={likeTotal} onLike={like} />}
+      />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    <Footer likeCount={likeTotal} ></Footer>
+
+  </BrowserRouter>
   );
 }
 
